9fd92a4502940f2c1ceb83183b936036552401bd
iWilli26 <willi.nguyen@hotmail.fr>
2022-06-30 14:09:32 +0200

    test pdf

--- facture.php
+++ facture.php
@@ -3,7 +3,7 @@ require('fpdf.php');
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial', 'B', 25);
-$pdf->Image('./logo.jpg', 10, 6, 30);
+$pdf->Image('./header.png');
 $pdf->Cell(80);
 $pdf->Cell(80, 30, 'INVOICE', 1, 0, 'C');
 $pdf->SetFont('Arial', 'B', 16);
