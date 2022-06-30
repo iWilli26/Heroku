60040a207b078aecc8e547b9ca5c5013f75b54dc
iWilli26 <willi.nguyen@hotmail.fr>
2022-06-30 15:27:00 +0200

    test pdf

--- facture.php
+++ facture.php
@@ -13,30 +13,29 @@ if ($conn->connect_error) {
     die("Connection to database failed: " . $conn->connect_error);
 }
 
-$id = $_GET['id'];
-$sql = "SELECT * FROM achats, products,facture WHERE facture.facture_id = " . $_GET['id'] . " AND facture.user_id=achats.user_id AND facture.date=achats.date AND achats.products_id=products.products_id";
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $array_values[] = $row;
-    }
-} else {
-    echo "0 results";
-}
+        $id = $_GET['id'];
+        $sql = "SELECT * FROM achats, products,facture WHERE facture.facture_id = " . $_GET['id'] . " AND facture.user_id=achats.user_id AND facture.date=achats.date AND achats.products_id=products.products_id";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $array_values[] = $row;
+            }
+        } else {
+            echo "0 results";
+        }
+ 
+        $res = json_encode($array_values);
+        echo $res;
+        $conn->close();
 
-$res = json_encode($array_values);
-
-echo $res;
-
-
-// require('fpdf.php');
-// $pdf = new FPDF();
-// $pdf->AddPage();
-// $pdf->SetMargins(0, 0, 0);
-// $pdf->SetFont('Arial', 'B', 25);
-// $pdf->Image('./header.png', 0, 0, 210, 40);
-// $pdf->Cell(210, 100, $res, 0, 1, 'C');
-// $pdf->Output();
+require('fpdf.php');
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetMargins(0, 0, 0);
+$pdf->SetFont('Arial', 'B', 25);
+$pdf->Image('./header.png', 0, 0, 210, 40);
+$pdf->Cell(210, 100, $res, 0, 1, 'C');
+$pdf->Output();
 
 
 $conn->close();
