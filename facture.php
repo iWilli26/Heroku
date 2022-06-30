4635a799132340c673ca73d52b07626cfd87d7f4
iWilli26 <willi.nguyen@hotmail.fr>
2022-06-30 14:50:55 +0200

    test pdf

--- facture.php
+++ facture.php
@@ -1,20 +1,21 @@
 <?php
-$host         = "127.0.0.1 (root)";
+echo $_GET['id'];	
+$host         = "localhost";
 $username     = "root";
 $password     = "";
 $dbname       = "heroku";
-$conn = mysqli_connect($host, $username, $password, $dbname);
-if ($conn->connect_error) {
-    echo 'fuck';
-}
-$sql = "SELECT * FROM achats, products,facture WHERE facture.facture_id= 1 AND facture.user_id=achats.user_id AND facture.date=achats.date AND achats.products_id=products.products_id";
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $array_values[] = $row;
-    }
-}
-echo json_encode($array_values);
+$conn = new mysqli($host, $username, $password, $dbname);
+// if ($conn->connect_error) {
+//     echo 'fuck';
+// }
+// $sql = "SELECT * FROM achats, products,facture WHERE facture.facture_id= 1 AND facture.user_id=achats.user_id AND facture.date=achats.date AND achats.products_id=products.products_id";
+// $result = mysqli_query($conn, $sql);
+// if (mysqli_num_rows($result) > 0) {
+//     while ($row = mysqli_fetch_assoc($result)) {
+//         $array_values[] = $row;
+//     }
+// }
+// echo json_encode($array_values);
 $conn->close();
 // require('fpdf.php');
 // $pdf = new FPDF();
