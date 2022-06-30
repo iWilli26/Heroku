215a52873e727f2c6099643461f581227e631031
iWilli26 <willi.nguyen@hotmail.fr>
2022-06-30 15:07:58 +0200

    test pdf

--- backend.php
+++ backend.php
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
 }
 switch ($_SERVER["REQUEST_METHOD"]) {
     case 'GET':
-        $sql = "SELECT * FROM user";
+        $sql = "SELECT * FROM achats, products,facture WHERE facture.facture_id= 1 AND facture.user_id=achats.user_id AND facture.date=achats.date AND achats.products_id=products.products_id";
         $result = mysqli_query($conn, $sql);
         if (mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
