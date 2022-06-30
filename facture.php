f450a554c773f2a4576f38be5f6ae179ca67a680
iWilli26 <willi.nguyen@hotmail.fr>
2022-06-30 14:43:23 +0200

    test pdf

--- facture.php
+++ facture.php
@@ -3,7 +3,7 @@ $host         = "localhost";
 $username     = "root";
 $password     = "";
 $dbname       = "heroku";
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = mysqli_connect($host, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection to database failed: " . $conn->connect_error);
 }
