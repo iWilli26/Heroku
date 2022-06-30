651e6bc6da29f44b4eac0fb5a0de7f2431b1b670
iWilli26 <willi.nguyen@hotmail.fr>
2022-06-30 15:04:04 +0200

    test pdf

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 const button = document.getElementById("test");
 button.addEventListener("click", function () {
-    console.log("test");
-    $.ajax({
-        method: "GET",
-        url: "./backend2.php",
-    }).done(function (data) {
-        console.log(data);
-    });
-
     $.ajax({
         method: "GET",
         url: "./backend.php",
