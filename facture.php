ceb437da1b6b89aad0b248debd0474619824e2bf
iWilli26 <willi.nguyen@hotmail.fr>
2022-06-30 15:35:17 +0200

    fetch in pdf

--- facture.php
+++ facture.php
@@ -25,7 +25,7 @@ if ($conn->connect_error) {
         }
  
         $res = $array_values;
-        // echo $res;
+        echo $res;
         $conn->close();
 
 require('fpdf.php');
@@ -34,7 +34,7 @@ $pdf->AddPage();
 $pdf->SetMargins(0, 0, 0);
 $pdf->SetFont('Arial', 'B', 11);
 $pdf->Image('./header.png', 0, 0, 210, 40);
-$pdf->Cell(210, 100, $res[0], 0, 1, 'C');
+$pdf->Cell(210, 100, $res, 0, 1, 'C');
 $pdf->Output();
 
 
