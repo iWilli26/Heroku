9f2e585296aa2a986c8d619edf3fdb5481abc0c2
iWilli26 <willi.nguyen@hotmail.fr>
2022-06-30 16:09:58 +0200

    fetch in pdf

--- facture.php
+++ facture.php
@@ -35,7 +35,7 @@ $pdf->SetMargins(0, 0, 0);
 $pdf->SetFont('Arial', 'B', 11);
 $pdf->Image('./header.png', 0, 0, 210, 40);
 $pdf->SetXY(0, 40);
-$pdf->MultiCell(100, 60, "Invoice to: \n" . $res[0]["nom"] . "\n " . $res[0]["adresse"], 1);
+$pdf->MultiCell(100, 60, "Invoice to: \n" . $res[0]["nom"].' '.$res[0]['prenom'] . "\n " . $res[0]["adresse"], 1);
 $pdf->Output();
 
 
