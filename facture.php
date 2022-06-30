c9f96642441db809fa2fe6cc8876391361e49f60
iWilli26 <willi.nguyen@hotmail.fr>
2022-06-30 16:06:44 +0200

    fetch in pdf

--- facture.php
+++ facture.php
@@ -13,20 +13,20 @@ if ($conn->connect_error) {
     die("Connection to database failed: " . $conn->connect_error);
 }
 
-        $id = $_GET['id'];
-        $sql = "SELECT * FROM achats, products,facture,user WHERE facture.facture_id = " . $_GET['id'] . " AND facture.user_id=achats.user_id AND facture.date=achats.date AND achats.products_id=products.products_id AND facture.user_id=user.user_id";
-        $result = mysqli_query($conn, $sql);
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                $array_values[] = $row;
-            }
-        } else {
-            echo "0 results";
-        }
- 
-        $res = $array_values;
-        // echo $res[0]['nom'];
-        $conn->close();
+$id = $_GET['id'];
+$sql = "SELECT * FROM achats, products,facture,user WHERE facture.facture_id = " . $_GET['id'] . " AND facture.user_id=achats.user_id AND facture.date=achats.date AND achats.products_id=products.products_id AND facture.user_id=user.user_id";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $array_values[] = $row;
+    }
+} else {
+    echo "0 results";
+}
+
+$res = $array_values;
+// echo $res[0]['nom'];
+$conn->close();
 
 require('fpdf.php');
 $pdf = new FPDF();
@@ -34,7 +34,7 @@ $pdf->AddPage();
 $pdf->SetMargins(0, 0, 0);
 $pdf->SetFont('Arial', 'B', 11);
 $pdf->Image('./header.png', 0, 0, 210, 40);
-$pdf->MultiCell(10, 60, 'Invoice to: \n'. $res[0]["nom"]. '\n '.$res[0]["adresse"], 1);
+$pdf->MultiCell(100, 60, 'Invoice to: \n' . $res[0]["nom"] . '\n ' . $res[0]["adresse"], 1);
 $pdf->Output();
 
 
