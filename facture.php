5852cbe8e7b67adb3c0422bfa7fe798f5be50c84
iWilli26 <willi.nguyen@hotmail.fr>
2022-06-30 15:24:44 +0200

    test pdf

--- facture.php
+++ facture.php
@@ -1,28 +1,42 @@
 <?php
-// echo $_GET['id'];	
-$host         = "localhost";
-$username     = "root";
-$password     = "";
-$dbname       = "heroku";
-$conn = new mysqli($host, $username, $password, $dbname);
+// Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 if ($conn->connect_error) {
-    echo 'fuck';
+    die("Connection to database failed: " . $conn->connect_error);
 }
-$sql = "SELECT * FROM achats, products,facture WHERE facture.facture_id= 1 AND facture.user_id=achats.user_id AND facture.date=achats.date AND achats.products_id=products.products_id";
+
+$id = $_GET['id'];
+$sql = "SELECT * FROM achats, products,facture WHERE facture.facture_id = " . $_GET['id'] . " AND facture.user_id=achats.user_id AND facture.date=achats.date AND achats.products_id=products.products_id";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
         $array_values[] = $row;
     }
+} else {
+    echo "0 results";
 }
-echo json_encode($array_values);
+
+$res = json_encode($array_values);
+
+echo $res;
+
+
+// require('fpdf.php');
+// $pdf = new FPDF();
+// $pdf->AddPage();
+// $pdf->SetMargins(0, 0, 0);
+// $pdf->SetFont('Arial', 'B', 25);
+// $pdf->Image('./header.png', 0, 0, 210, 40);
+// $pdf->Cell(210, 100, $res, 0, 1, 'C');
+// $pdf->Output();
+
+
 $conn->close();
-
-
-require('fpdf.php');
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetMargins(0, 0, 0);
-$pdf->SetFont('Arial', 'B', 25);
-$pdf->Image('./header.png', 0, 0, 210, 40);
-$pdf->Output();
