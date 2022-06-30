8cf572fcc8040e669dcef956c6773c9948d0cd01
iWilli26 <willi.nguyen@hotmail.fr>
2022-06-30 14:03:52 +0200

    test pdf

--- facture.php
+++ facture.php
@@ -1,39 +1,12 @@
 <?php
-    // Get Heroku ClearDB connection information
-    // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    // $cleardb_server = $cleardb_url["host"];
-    // $cleardb_username = $cleardb_url["user"];
-    // $cleardb_password = $cleardb_url["pass"];
-    // $cleardb_db = substr($cleardb_url["path"], 1);
-    // $active_group = 'default';
-    // $query_builder = TRUE;
-    // // Connect to DB
-    // $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-    // if ($conn->connect_error) {
-    //     die("Connection to database failed: " . $conn->connect_error);
-    // }
-    // switch ($_SERVER["REQUEST_METHOD"]) {
-    //     case 'GET':
-    //         $sql = "SELECT * FROM user";
-    //         $result = mysqli_query($conn, $sql);
-    //         if (mysqli_num_rows($result) > 0) {
-    //             while ($row = mysqli_fetch_assoc($result)) {
-    //                 $array_values[] = $row;
-    //             }
-    //         } else {
-    //             echo "0 results";
-    //         }
-    //         $test = json_encode($array_values);
-    //         $conn->close();
-    //         break;
-    //     }
 
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial', 'B', 25);
-// $pdf->Image('logo.jpg', 10, 6, 30);
+$pdf->Image('./logo.jpg', 10, 6, 30);
 $pdf->Cell(80);
 $pdf->Cell(80, 30, 'INVOICE', 1, 0, 'C');
 $pdf->SetFont('Arial', 'B', 16);
 $pdf->Cell(60, 30, 'Brand name', 1);
 $pdf->Output();
+?>
