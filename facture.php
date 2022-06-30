cae19a03c3b644b007ce36eab78ab190429d4d00
iWilli26 <willi.nguyen@hotmail.fr>
2022-06-30 11:38:07 +0200

    test pdf

--- facture.php
+++ facture.php
@@ -3,6 +3,6 @@ require('fpdf.php');
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial', 'B', 16);
-$pdf->Cell(40, 10, 'Hello World !', 1, 0, 'C');
+// $pdf->Cell(40, 10, 'Hello World !', 1, 0, 'C');
 $pdf->Cell(60, 20, 'Hello World !', 1);
 $pdf->Output();
