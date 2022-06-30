e906f9a2fdbefb20f74cffefa4c363c046040ff2
iWilli26 <willi.nguyen@hotmail.fr>
2022-06-30 13:03:07 +0200

    test pdf

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import axios from "axios";
 const button = document.getElementById("button");
 button.addEventListener("click", function () {
     console.log("test");
-    axios.get("./backend.php").then((resp) => {
-        console.log(resp.data);
+    $.ajax({
+        method: "GET",
+        url: "./backend",
+    }).done(function (data) {
+        console.log(data);
     });
 });
