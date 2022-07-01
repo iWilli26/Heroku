f239839af69f296922148527d4c6b013a72f33c8
iWilli26 <willi.nguyen@hotmail.fr>
2022-07-01 12:42:39 +0200

    fetch in pdf

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
 <?php
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 1);
-$active_group = 'default';
-$query_builder = TRUE;
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db = substr($cleardb_url["path"], 1);
+// $active_group = 'default';
+// $query_builder = TRUE;
+// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+// if ($conn->connect_error) {
+//     die("Connection to database failed: " . $conn->connect_error);
+// }
+// $sql = "SELECT * FROM facture WHERE 1";
+// if (mysqli_num_rows($result) > 0) {
+//     while ($row = mysqli_fetch_assoc($result)) {
+//         $array_values[] = $row;
+//     }
+// } else {
+//     echo "0 results";
+// }
 
-if ($conn->connect_error) {
-    die("Connection to database failed: " . $conn->connect_error);
-}
+// $res = $array_values;
+// $conn->close();
 
-$sql = "SELECT * FROM `facture` WHERE 1";
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $array_values[] = $row;
-    }
-} else {
-    echo "0 results";
-}
-
-$res = $array_values;
-$conn->close();
 ?>
 <html lang="en">
 
@@ -32,7 +31,6 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="/index.js" defer></script>
-
     <title>HAHAHAHAHAHAHAHAHAHAA</title>
 </head>
 
@@ -40,7 +38,7 @@ $conn->close();
     <?php
     // $element = '<a href="https://hdm-fpdf.herokuapp.com/facture.php?id=".$res[0]["facture_id"]>Facture' . $res[0]["facture_id"] . '</a>';
     for ($i; $i < count($res); $i++) {
-        echo $i;
+        echo 'pute';
     }
 
 
