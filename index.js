4d06b12665b5e9d4d855bda65d3149540cd3e3ff
iWilli26 <willi.nguyen@hotmail.fr>
2022-07-01 12:29:53 +0200

    fetch in pdf

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const button = document.getElementById("test");
 button.addEventListener("click", function () {
     $.ajax({
         method: "GET",
-        url: "./backend.php",
+        url: "./backend.php?id=1",
     }).done(function (data) {
         console.log(data);
     });
